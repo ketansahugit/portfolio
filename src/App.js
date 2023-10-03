fe585609bcf4097b567850abc55e4c70775ea750
Ketan Sahu <rorozoro9888@gmail.com>
2023-10-03 18:41:32 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Body = styled.div`
+background-color: ${({ theme })}
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='App-header'>
+        Portfolio
       </header>
     </div>
   );
